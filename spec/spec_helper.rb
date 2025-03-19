94315b9b43cc0a247266696256ba53cf99dadfad
Kematzy <kematzy@gmail.com>
2025-03-19 08:18:58 +0800

    Update spec/spec_helper & test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,40 @@
 
 ENV['RACK_ENV'] = 'test'
 
+# Enable coverage tracking when the 'COVERAGE' environment variable is set
 if ENV['COVERAGE']
-  require File.join(File.dirname(File.expand_path(__FILE__)), 'minitest_have_tags_coverage')
-  SimpleCov.minitest_have_tags_coverage
+  require 'simplecov'
+
+  SimpleCov.start do
+    # ignore the test files
+    add_filter '/spec/'
+    # ignore version testing due to loading in the `.gemspec` file causes issues.
+    add_filter 'lib/minitest/have_tag/version.rb'
+
+    # track all library files
+    track_files 'lib/**/*.rb'
+  end
 end
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+# Ensure the library path is in the load path
+$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
 
-require 'rubygems'
+# # Set up Bundler before loading gems
+# require 'bundler/setup'
+
+# Fix duplicate test runs when using Rake
+require 'minitest'
+Minitest::Runnable.runnables.clear
+
+# Load Minitest
 require 'minitest/autorun'
+require 'minitest/spec'
 require 'minitest/assert_errors'
-require 'minitest/have_tag'
 # require 'minitest/hooks/default'
+# require 'mocha/minitest'
+
+# Load the code being tested
+require 'minitest/have_tag'
+
+# Add coloured output
 require 'minitest/rg'
