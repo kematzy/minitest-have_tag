fffe1dcf3bfbc2a5d710b9e289892df916e5a370
Kematzy <kematzy@gmail.com>
2025-03-19 08:29:51 +0800

    Restructure & cleanup tests (II)

--- spec/minitest/assertions_spec.rb
+++ spec/minitest/assertions_spec.rb
@@ -198,6 +198,166 @@ describe 'Minitest' do
     end
     # /#assert_have_tag(:html, :tag)
 
+    describe '#refute_have_tag(:html, :tag)' do
+      describe 'basic validation' do
+        it 'handles nil values' do
+          assert_no_error { refute_have_tag(nil, 'br') }
+        end
+
+        it 'handles empty strings' do
+          assert_no_error { refute_have_tag(nil, 'br') }
+        end
+
+        it 'reports when tags that should not exist do exist' do
+          msg = 'Expected "<br>" to NOT have tag "br", but such a tag was found'
+          assert_returns_error(msg) { refute_have_tag('<br>', 'br') }
+        end
+
+        it 'validates when expected tags are not found' do
+          assert_no_error { refute_have_tag('<br>', 'brr') }
+        end
+      end
+
+      describe 'tag attributes' do
+        describe 'simple attribute validation' do
+          it 'reports when tags with unwanted attributes exist' do
+            msg = [
+              'Expected "<hr class=\"divider\">" to NOT have tag "hr[class]", ',
+              'but such a tag was found'
+            ].join
+
+            assert_returns_error(msg) do
+              refute_have_tag('<hr class="divider">', 'hr[class]')
+            end
+
+            msg = [
+              'Expected "<hr class=\\"divider\\">" to NOT have tag "hr[class=divider]", ',
+              'but such a tag was found'
+            ].join
+
+            assert_returns_error(msg) do
+              refute_have_tag('<hr class="divider">', 'hr[class=divider]')
+            end
+          end
+
+          it 'validates when attribute selectors do not match' do
+            assert_no_error { refute_have_tag('<hr class="divider">', 'hr[classs]') }
+            assert_no_error { refute_have_tag('<hr class="divider">', 'hr[class=divder]') }
+          end
+        end
+
+        describe 'complex attribute validation' do
+          it 'reports when unwanted complex selectors match' do
+            html = '<div id="header" class="row"></div>'
+            msg = [
+              'Expected "<div id=\"header\" class=\"row\"></div>" to NOT have tag "TAG", ',
+              'but such a tag was found'
+            ].join
+
+            [
+              'div#header',
+              'div[id=header]',
+              'div.row',
+              'div[class=row]',
+              'div#header.row',
+              'div[id=header][class=row]'
+            ].each do |selector|
+              assert_returns_error(msg.sub('TAG', selector)) { refute_have_tag(html, selector) }
+            end
+          end
+
+          it 'reports when unwanted multiple class selectors match' do
+            html = '<div id="header" class="row columns"></div>'
+            msg = [
+              'Expected "<div id=\"header\" class=\"row columns\"></div>" to NOT have ',
+              'tag "TAG", but such a tag was found'
+            ].join
+
+            [
+              'div[class=\'row columns\']',
+              'div#header.row.columns',
+              'div[id=header][class=\'row columns\']'
+            ].each do |selector|
+              assert_returns_error(msg.sub('TAG', selector)) { refute_have_tag(html, selector) }
+            end
+          end
+
+          it 'validates when complex selectors do not match' do
+            html = '<div id="header" class="row"></div>'
+
+            assert_no_error { refute_have_tag(html, 'div#headers') }
+            assert_no_error { refute_have_tag(html, 'div#header.rows') }
+          end
+        end
+      end
+
+      describe 'tag contents' do
+        describe 'string content validation' do
+          it 'reports when unwanted content matches' do
+            html = %(<label for="name">Name:</label>)
+            msg = [
+              'Expected "<label for=\"name\">Name:</label>" to NOT have tag ',
+              '"label[for=name]", but such a tag was found'
+            ].join
+
+            assert_returns_error(msg) { refute_have_tag(html, 'label[for=name]', 'Name:') }
+
+            html = %(<label for="name">User Name:</label>)
+            msg = [
+              'Expected "<label for=\"name\">User Name:</label>" to NOT have tag ',
+              '"label[for=name]", but such a tag was found'
+            ].join
+
+            assert_returns_error(msg) { refute_have_tag(html, 'label[for=name]', 'User Name:') }
+          end
+
+          it 'handles nil content appropriately' do
+            assert_returns_error(/to NOT have tag "label\[for=name\]", but such a tag was found/) do
+              refute_have_tag('<label for="name">Name:</label>', 'label[for=name]', nil)
+            end
+          end
+
+          it 'handles empty content validation' do
+            html = %(<label for="name">Username:</label>)
+            assert_no_error { refute_have_tag(html, 'label[for=name]', '') }
+
+            assert_returns_error(/to NOT have tag "label\[for=name\]", but such a tag was found/) do
+              refute_have_tag('<label for="name"></label>', 'label[for=name]', '')
+            end
+
+            assert_no_error do
+              refute_have_tag('<label for="name">contents</label>', 'label[for=name]', '')
+            end
+          end
+
+          it 'properly handles non-string/non-regex content parameters' do
+            html = %(<label for="name"></label>\n)
+            assert_no_error { refute_have_tag(html, 'label[for=name]', :symbol) }
+          end
+        end
+
+        describe 'regex content validation' do
+          it 'reports when unwanted regex content matches' do
+            html = %(<label for="name">Username:</label>\n)
+            msg = [
+              'Expected "<label for=\\"name\\">Username:</label>\n" to NOT have tag ',
+              '"label[for=name]", but such a tag was found'
+            ].join
+
+            assert_returns_error(msg) { refute_have_tag(html, 'label[for=name]', /User/) }
+            assert_returns_error(msg) { refute_have_tag(html, 'label[for=name]', /user/i) }
+          end
+
+          it 'validates when regex patterns do not match content' do
+            html = '<label for="name">Username:</label>'
+
+            assert_no_error { refute_have_tag(html, 'label[for=name]', /Users/) }
+            assert_no_error { refute_have_tag(html, 'label[for=name]', /users/i) }
+          end
+        end
+      end
+    end
+    # /#refute_have_tag(:html, :tag)
   end
   # /::Assertions
 end
